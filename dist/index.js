51ea912a320340dd26f525bb361acc86969b79af
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-10-21 11:05:14 +0900

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -68,7 +68,7 @@ function exportAttachments(parser, activity) {
                             yield exec.exec('curl', [
                                 '-X',
                                 'POST',
-                                'https://xcresulttool-file.herokuapp.com/file',
+                                '//xcresulttool-file.herokuapp.com/file',
                                 '-d',
                                 image.toString('base64')
                             ], options);
